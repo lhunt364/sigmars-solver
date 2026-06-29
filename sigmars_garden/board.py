23cfc325500d21461ca48fa0641fd77a8c5115b8
Logan Hunt <loganhunt364@gmail.com>
2026-06-29 01:30:50 -0400

    added an enum for tile types (each kind of marble plus an empty state) and created structures to later be used to identify if a tile can be matched with a salt, ie its an element, and a tuple with the metals for keeping track of the sequence they have to be matched in

--- sigmars_garden/board.py
+++ sigmars_garden/board.py
@@ -7,6 +7,7 @@ Board cell representation class and coordinate helpers
 from __future__ import annotations
 from collections.abc import Iterator
 from dataclasses import dataclass
+from enum import Enum, auto
 
 
 @dataclass(frozen=True) # makes __init__ and stuff, frozen makes fields immutable
@@ -39,12 +40,11 @@ class Hex:
         return self + AXIAL_DIRECTIONS[direction % 6]
 
 
-
 """
   3 4
 2  S  5
   1 6
-The six axial directions as Hex's that can be added to Hex to find cells 1 unit away in each direction.
+The six axial directions as Hex's that can be added to a Hex to find cells 1 unit away in each direction.
 They are arranged in order, so consecutive entries correspond to consecutive neighboring cells.
 """
 AXIAL_DIRECTIONS: tuple[Hex, ...] = (
@@ -53,5 +53,46 @@ AXIAL_DIRECTIONS: tuple[Hex, ...] = (
     Hex(0, -1),
     Hex(-1, 0),
     Hex(-1, 1),
-    Hex(0, 1), # 6
+    Hex(0, 1) # 6
+)
+
+
+class TileType(Enum):
+    """
+    The possible types of spaces, including empty
+    """
+    AIR = auto()
+    FIRE = auto()
+    WATER = auto()
+    EARTH = auto()
+    SALT = auto()
+ 
+    VITAE = auto()
+    MORS = auto()
+ 
+    QUICKSILVER = auto()
+    LEAD = auto()
+    TIN = auto()
+    IRON = auto()
+    COPPER = auto()
+    SILVER = auto()
+    GOLD = auto()
+
+    EMPTY = auto()
+
+
+# The four cardinal elements as a set
+ELEMENTS: frozenset[TileType] = frozenset(
+    {TileType.AIR, TileType.FIRE, TileType.WATER, TileType.EARTH}
+)
+
+
+# The metals in the order that they have to be matched
+METAL_SEQUENCE: tuple[TileType, ...] = (
+    TileType.LEAD,
+    TileType.TIN,
+    TileType.IRON,
+    TileType.COPPER,
+    TileType.SILVER,
+    TileType.GOLD
 )
