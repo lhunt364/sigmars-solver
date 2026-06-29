f53b36d78f8f7c53ea1f47b290325c39f039621d
Logan Hunt <loganhunt364@gmail.com>
2026-06-29 01:02:27 -0400

    added arithmetic helpers and a tuple for a unit step in each axial direction as a Hex

--- sigmars_garden/board.py
+++ sigmars_garden/board.py
@@ -22,4 +22,25 @@ class Hex:
     def s(self) -> int:
         return -self.q - self.r
     
+    def __add__(self, other: Hex) -> Hex:
+        return Hex(self.q + other.q, self.r + other.r)
     
+    def __sub__(self, other: Hex) -> Hex:
+        return Hex(self.q - other.q, self.r - other.r)
+
+
+"""
+  3 4
+2  S  5
+  1 6
+The six axial directions as Hex's that can be added to Hex to find cells 1 unit away in each direction.
+They are arranged in order, so consecutive entries correspond to consecutive neighboring cells.
+"""
+AXIAL_DIRECTIONS: tuple[Hex, ...] = (
+    Hex(1, 0), # 1
+    Hex(1, -1),
+    Hex(0, -1),
+    Hex(-1, 0),
+    Hex(-1, 1),
+    Hex(0, 1), # 6
+)
